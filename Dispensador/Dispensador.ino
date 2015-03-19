fd6f6a5b278efc538d31710f1993d3943d677f83
MeletChirino <melet.chirino@gmail.com>
2015-03-19 13:24:32 -0500

    Se integro el LCD
    Ya funciona el LCD, pero solo lee el UID, falta que lea los
    interruptores y los mensajes guardados en la carpeta.

--- Dispensador/Dispensador.ino
+++ Dispensador/Dispensador.ino
@@ -1,28 +1,44 @@
-int i;
-int a[4];
+#include <SPI.h>
+#include "PN532_SPI.h"
+#include "PN532.h"
+#include "NfcAdapter.h"
+
+PN532_SPI interface(SPI, 10); // create a SPI interface for the shield with the SPI CS terminal at digital pin 10
+
+NfcAdapter nfc = NfcAdapter(interface); // create an NFC adapter object
+ // include the library code:
+#include <LiquidCrystal.h>
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
+String rec;
+char chab[20];
 void setup() {
-  Serial.begin(9600);
-  interrupts(); // put your setup code here, to run once:
-  for (i = 2; i <= 7; i++) {
-    pinMode(i, INPUT);
-  }
-  attachInterrupt(1, ki, RISING);
+Serial.begin(115200); // start serial comm
+    Serial.println("NDEF Reader");
+    nfc.begin(); // begin NFC comm
+      // set up the LCD's number of columns and rows: 
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.clear();
+  lcd.write("NDEF Reader Enabled");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-}
-void ki() {
-  Serial.println("Interrupcion exitosa");
-  int j = 0;
-  for (i = 3; i <= 6; i++)
-  { if (digitalRead(i) == HIGH) {
-      a[j] = 1;
-    }    else {
-      a[j] = 0;
-    } j = j + 1;
+  if (nfc.tagPresent()) // Do an NFC scan to see if an NFC tag is present
+  {
+    lcd.clear();
+    NfcTag tag = nfc.read();
+    Serial.println(tag.getTagType());
+    Serial.print("UID: ");Serial.println(tag.getUidString());
+    rec=tag.getUidString();
+    rec.toCharArray(chab, 12);
+    lcd.setCursor(0,1);
+    lcd.write("UID:");
+    for(int i=0;i<=10;i++){
+      lcd.write(chab[i]);
+    }
+    lcd.setCursor(0,0);
+    lcd.write("User Registered");
   }
-  Serial.print(a[0]); Serial.print(a[1]); Serial.print(a[2]); Serial.print(a[3]);
-  Serial.write("\n \n \n");
+delay(1000);
 }
